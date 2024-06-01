3048e35a4edd89b61972c80a2af068df6095db3a
Franco Diaz Luna <diazlunafrancoe@gmail.com>
2024-05-31 21:05:52 -0300

    fixes

--- BonvinoApp/CapaNegocio/GestorGeneracionRankingVino.cs
+++ BonvinoApp/CapaNegocio/GestorGeneracionRankingVino.cs
@@ -2,9 +2,6 @@
 using BonvinoApp.CapaPresentacion.Forms;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BonvinoApp.CapaNegocio.Gestores
 {
